fb1a3fedefc12c85c7269f3b8c892df5b047e660
Benjamin Manns <benmanns@gmail.com>
2011-02-16 20:18:28 -0500

    Added five minute cache for /pages/launcher/na.

--- lib/launching.rb
+++ lib/launching.rb
@@ -2,17 +2,23 @@ module Launching
   class Application < Sinatra::Base
     configure do
       require 'open-uri'
+
+      CACHE = Dalli::Client.new
     end
 
     get '/pages/launcher/na' do
       content_type :js
-      halt 500 unless open('http://ll.leagueoflegends.com/pages/launcher/na').read =~ /refreshContent\((.*)\);/
-      data = JSON.parse $1
-      data['status'] = true
-      data['serverStatus'] = 1
-      data['news'].unshift({ 'title' => 'Brought to you by Benjamin Manns', 'url' => 'http://benmanns.com', 'date' => Time.new.strftime('%m.%d.%Y') })
-      data['community'].unshift({ 'title' => 'Brought to you by Benjamin Manns', 'url' => 'http://benmanns.com' })
-      "\r\nrefreshContent(#{data.to_json});"
+      unless content = CACHE.get('/pages/launcher/na')
+        halt 500 unless open('http://ll.leagueoflegends.com/pages/launcher/na').read =~ /refreshContent\((.*)\);/
+        data = JSON.parse $1
+        data['status'] = true
+        data['serverStatus'] = 1
+        data['news'].unshift({ 'title' => 'Brought to you by Benjamin Manns', 'url' => 'http://benmanns.com', 'date' => Time.new.strftime('%m.%d.%Y') })
+        data['community'].unshift({ 'title' => 'Brought to you by Benjamin Manns', 'url' => 'http://benmanns.com' })
+        content = "\r\nrefreshContent(#{data.to_json});"
+        CACHE.set '/pages/launcher/na', content, 300
+      end
+      content
     end
 
     get '/services/connection_info' do
